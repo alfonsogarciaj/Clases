6569b1573eb6f4cf0973065824563a42b469073c
alfonsogj <alfonsogjorge@gmail.com>
2022-03-29 20:16:18 +0100

    añado

---
+++ ETS/primer-proyecto-test/src/test/java/es/iespuerto/ets/AppTest.java
@@ -0,0 +1,14 @@
+package es.iespuerto.ets;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class AppTest {
+
+    @Test
+    void testApp() {
+        assertEquals(1 ,1 ,"Da error");
+    }
+
+}
