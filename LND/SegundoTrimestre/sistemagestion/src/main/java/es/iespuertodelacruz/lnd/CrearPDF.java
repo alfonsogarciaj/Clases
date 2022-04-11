eccbfb71ad314af38317b26bfe5aab81ad6995af
alfonsogj14 <alfonsogjorge@gmail.com>
2022-04-11 17:22:48 +0100

    añado

--- LND/SegundoTrimestre/sistemagestion/src/main/java/es/iespuertodelacruz/lnd/CrearPDF.java
+++ LND/SegundoTrimestre/sistemagestion/src/main/java/es/iespuertodelacruz/lnd/CrearPDF.java
@@ -13,48 +13,55 @@ import org.apache.pdfbox.pdmodel.graphics.image.PDImageXObject;
 public class CrearPDF{
     public static void main(String[] args) throws IOException{
             try (PDDocument documento = new PDDocument()) {
-                Scanner sc = new Scanner(System.in);
-                boolean salir = false;
-                int opcion;
-                while (!salir) {
-                    System.out.println("1. Crear documento con titulo e imagen");
-                    System.out.println("2. Crear documento solo con texto ");
-                    System.out.println("3. Crear documento solo con imagen");
-                    System.out.println("4. Salir");
-                    System.out.println("Escribe una de las opciones");
-                    opcion = sc.nextInt();
-                    switch (opcion) {
-                        case 1:
-                            PDPage page = CrearPagina();
-                            documento.addPage(page);
-                            System.out.println("Has seleccionado la opcion 1");
-                            System.out.println("Introduce un titulo para el PDF");
-                            String titulo = sc.nextLine();
-                            PDPageContentStream content = CrearTitulo(documento, page, titulo);
-                            System.out.println(
-                            "Ahora debe indicar el nombre de la imagen que quiere poner(Recuerde la imagen debe estar dentro de resources)");
-                            String nombreDeImagen = sc.nextLine();
-                            PDImageXObject ponerImagen = PonerImagen(documento, nombreDeImagen);
-                            content.drawImage(ponerImagen, 20, 20, ponerImagen.getWidth() / 3, ponerImagen.getHeight() / 3);
-                            content.close();
-                            System.out.println("Ponga un nombre al documento");
-                            String nombrePDF = sc.nextLine();
-                            documento.save(nombrePDF + ".pdf");
+                try (Scanner sc = new Scanner(System.in)) {
+                    boolean salir = false;
+                    int opcion;
+                    while (!salir) {
+                        System.out.println("1. Crear documento con titulo e imagen");
+                        System.out.println("2. Crear documento solo con texto ");
+                        System.out.println("3. Salir");
+                        System.out.println("Escribe una de las opciones");
+                        opcion = sc.nextInt();
+                        switch (opcion) {
+                            case 1:
+                                PDPage page = CrearPagina();
+                                documento.addPage(page);
+                                System.out.println("Has seleccionado la opcion 1");
+                                System.out.println("Introduce un titulo para el PDF");
+                                String titulo = sc.nextLine();
+                                PDPageContentStream content = CrearTitulo(documento, page, titulo);
+                                System.out.println(
+                                "Ahora debe indicar el nombre de la imagen que quiere poner(Recuerde la imagen debe estar dentro de resources)");
+                                String nombreDeImagen = sc.nextLine();
+                                PDImageXObject ponerImagen = PonerImagen(documento, nombreDeImagen);
+                                content.drawImage(ponerImagen, 20, 20, ponerImagen.getWidth() / 3, ponerImagen.getHeight() / 3);
+                                content.close();
+                                System.out.println("Ponga un nombre al documento");
+                                String nombrePDF = sc.nextLine();
+                                documento.save(nombrePDF + ".pdf");
 
-                            break;
-                        case 2:
-                            System.out.println("Has seleccionado la opcion 2");
-                            break;
-                        case 3:
-                            System.out.println("Has seleccionado la opcion 3");
-                            break;
-                        case 4:
-                            salir = true;
-                            break;
-                        default:
-                            System.out.println("Solo números entre 1 y 4");
+                                break;
+                            case 2:
+                                System.out.println("Has seleccionado la opcion 2");
+                                PDPage page2 = CrearPagina();
+                                documento.addPage(page2);
+                                System.out.println("Has seleccionado la opcion 1");
+                                System.out.println("Introduce un titulo para el PDF");
+                                String titulo2 = sc.nextLine();
+                                PDPageContentStream content2 = CrearTitulo(documento, page2, titulo2);
+                                content2.close();
+                                System.out.println("Ponga un nombre al documento");
+                                String nombrePDF2 = sc.nextLine();
+                                documento.save(nombrePDF2 + ".pdf");
+                                break;
+                            case 3:
+                                salir = true;
+                                break;
+                            default:
+                                System.out.println("Solo números entre 1 y 3");
+                        }
+
                     }
-
                 }
             }
         }
