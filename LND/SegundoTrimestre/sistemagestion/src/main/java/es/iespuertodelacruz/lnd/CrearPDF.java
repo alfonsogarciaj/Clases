e33255cc51d7633288c81fdfbdc0d108ad8d9b79
alfonsogj14 <alfonsogjorge@gmail.com>
2022-04-13 16:06:18 +0100

    añado

--- LND/SegundoTrimestre/sistemagestion/src/main/java/es/iespuertodelacruz/lnd/CrearPDF.java
+++ LND/SegundoTrimestre/sistemagestion/src/main/java/es/iespuertodelacruz/lnd/CrearPDF.java
@@ -14,51 +14,44 @@ public class CrearPDF {
     public static void main(String[] args) throws IOException {
         try (PDDocument documento = new PDDocument()) {
             try (Scanner sc = new Scanner(System.in)) {
-                boolean salir = false;
                 int opcion;
-                while (!salir) {
-                    System.out.println("1. Crear documento con titulo e imagen");
-                    System.out.println("2. Crear documento solo con texto");
-                    System.out.println("3. Salir");
-                    System.out.println("Escribe una de las opciones");
-                    opcion = sc.nextInt();sc.nextLine();
-                    switch (opcion) {
-                        case 1:
-                            PDPage page = CrearPagina();
-                            documento.addPage(page);
-                            System.out.println("Has seleccionado la opcion 1");
-                            System.out.println("Introduce un titulo para el PDF");
-                            String titulo = sc.nextLine();
-                            PDPageContentStream content = CrearTitulo(documento, page, titulo);
-                            System.out.println(
-                                    "Ahora debe indicar el nombre de la imagen y su formato(Recuerde la imagen debe estar dentro de resources)");
-                            String posImagen = sc.nextLine();
-                            PDImageXObject ponerImagen = PonerImagen(documento, posImagen);
-                            content.drawImage(ponerImagen, 80, 120, ponerImagen.getWidth() / 3,
-                                    ponerImagen.getHeight() / 3);
-                            content.close();
-                            System.out.println("Ponga un nombre al documento");
-                            String nombrePDF = sc.nextLine();
-                            documento.save(nombrePDF + ".pdf");
-                            break;
-                        case 2:
-                            System.out.println("Has seleccionado la opcion 2");
-                            PDPage page2 = CrearPagina();
-                            documento.addPage(page2);
-                            System.out.println("Introduce un titulo para el PDF");
-                            String titulo2 = sc.nextLine();
-                            PDPageContentStream content2 = CrearTitulo(documento, page2, titulo2);
-                            content2.close();
-                            System.out.println("Ponga un nombre al documento");
-                            String nombrePDF2 = sc.nextLine();
-                            documento.save(nombrePDF2 + ".pdf");
-                            break;
-                        case 3:
-                            salir = true;
-                            break;
-                        default:
-                            System.out.println("Solo números entre 1 y 3");
-                    }
+                System.out.println("1. Crear documento con titulo e imagen");
+                System.out.println("2. Crear documento solo con texto");
+                System.out.println("Escribe una de las opciones");
+                opcion = sc.nextInt();sc.nextLine();
+                switch (opcion) {
+                    case 1:
+                        PDPage page = CrearPagina();
+                        documento.addPage(page);
+                        System.out.println("Has seleccionado la opcion 1");
+                        System.out.println("Introduce un titulo para el PDF");
+                        String titulo = sc.nextLine();
+                        PDPageContentStream content = CrearTitulo(documento, page, titulo);
+                        System.out.println(
+                                "Ahora debe indicar el nombre de la imagen y su formato(Recuerde la imagen debe estar dentro de resources)");
+                        String posImagen = sc.nextLine();
+                        PDImageXObject ponerImagen = PonerImagen(documento, posImagen);
+                        content.drawImage(ponerImagen, 80, 120, ponerImagen.getWidth() / 3,
+                                ponerImagen.getHeight() / 3);
+                        content.close();
+                        System.out.println("Ponga un nombre al documento");
+                        String nombrePDF = sc.nextLine();
+                        documento.save(nombrePDF + ".pdf");
+                        break;
+                    case 2:
+                        System.out.println("Has seleccionado la opcion 2");
+                        PDPage page2 = CrearPagina();
+                        documento.addPage(page2);
+                        System.out.println("Introduce un texto para el PDF");
+                        String titulo2 = sc.nextLine();
+                        PDPageContentStream content2 = CrearTexto(documento, page2, titulo2);
+                        content2.close();
+                        System.out.println("Ponga un nombre al documento");
+                        String nombrePDF2 = sc.nextLine();
+                        documento.save(nombrePDF2 + ".pdf");
+                        break;
+                    default:
+                        System.out.println("Solo números entre 1 y 3");
                 }
             }
         }
@@ -95,7 +88,8 @@ public class CrearPDF {
 
     /**
      * Metodo para incorporar una imagen que desea el usuario
-     * @param documento documento con el texto creado para introducir la imagen
+     * 
+     * @param documento      documento con el texto creado para introducir la imagen
      * @param nombreDeImagen de la imagen que desee el usuario
      * @return documento con la imagen
      * @throws IOException
@@ -104,5 +98,24 @@ public class CrearPDF {
         PDImageXObject imagen = PDImageXObject.createFromByteArray(documento,
                 CrearPDF.class.getResourceAsStream("/" + posImagen).readAllBytes(), posImagen);
         return imagen;
+    }
+
+        /**
+     * Metodo para crear el texto que desea introducir el usuario
+     * 
+     * @param documento que se le envia al creador de texto
+     * @param page      pagina del documento
+     * @param titulo    que le envia el usuario para el documento
+     * @return devuelve el documento creado con el texto del usuario
+     * @throws IOException
+     */
+    public static PDPageContentStream CrearTexto(PDDocument documento, PDPage page, String titulo) throws IOException {
+        PDPageContentStream content = new PDPageContentStream(documento, page);
+        content.beginText();
+        content.setFont(PDType1Font.TIMES_BOLD, 7);
+        content.newLineAtOffset(15, page.getMediaBox().getHeight() - 32);
+        content.showText(titulo);
+        content.endText();
+        return content;
     }
 }
--- LND/SegundoTrimestre/sistemagestion/src/test/java/es/iespuertodelacruz/lnd/AppTest.java
+++ LND/SegundoTrimestre/sistemagestion/src/test/java/es/iespuertodelacruz/lnd/AppTest.java
@@ -3,16 +3,37 @@ package es.iespuertodelacruz.lnd;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 /**
  * Unit test for simple App.
  */
 class AppTest {
-    /**
-     * Rigorous Test.
-     */
+
+    CrearPDF crearPDF;
     @Test
     void testApp() {
         assertEquals(1, 1);
+    }
+
+    @Test
+    public void crearPDF(){
+        crearPDF = new CrearPDF();
+        assertNotNull(crearPDF, "La instancia de PDF es nulo");
+    }
+
+    @Test
+    public void CrearPagina(){
+        assertNotNull(crearPDF, "La instancia de crear Page es nula");    
+    }
+
+    @Test
+    public void CrearTitulo(){
+        assertNotNull(crearPDF, "La instancia de crear titulo es nula");
+    }
+
+    @Test
+    public void PonerImagen(){
+        assertNotNull(crearPDF,"La instancia de poner imagen es nula");
     }
 }
