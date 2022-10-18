3a7fecd7c9692a525d74d93ae6baa074746977a5
Alfonso García Jorge <alfonsogjorge@gmail.com>
2022-10-18 19:31:02 +0100

    actividad ut2

--- Programación/UT2Identificación de los elementos de un programa informático (Python)/pythonProject/Ejercicios1.py
+++ Programación/UT2Identificación de los elementos de un programa informático (Python)/pythonProject/Ejercicios1.py
@@ -1,3 +1,9 @@
-print(3**(1//2))
-print(4**1//2)
+a = 4
+b = 7
 
+aBotellasPrecioTotal = a * 100.23
+bBotellasPrecioTotal = b * 60.25
+botellasPrecioTotal = aBotellasPrecioTotal + bBotellasPrecioTotal
+mediaPrecioBotella = botellasPrecioTotal / a + b
+print(botellasPrecioTotal)
+print(mediaPrecioBotella)
