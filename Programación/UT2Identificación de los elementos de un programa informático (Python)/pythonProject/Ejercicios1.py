a4ec751a39032d3eba9fd13c657b1d4b34c48d88
Alfonso García Jorge <alfonsogjorge@gmail.com>
2022-10-24 10:24:39 +0100

    UT2 actividad

--- Programación/UT2Identificación de los elementos de un programa informático (Python)/pythonProject/Ejercicios1.py
+++ Programación/UT2Identificación de los elementos de un programa informático (Python)/pythonProject/Ejercicios1.py
@@ -1,11 +1,3 @@
-a = 4
-b = 7
-
-aBotellasPrecio = 100.23
-bBotellasPrecio = 60.25
-aBotellasPrecioTotal = a * aBotellasPrecio
-bBotellasPrecioTotal = b * bBotellasPrecio
-botellasPrecioTotal = aBotellasPrecioTotal + bBotellasPrecioTotal
-mediaPrecioBotella = round(botellasPrecioTotal / (a + b),2)
-
-print(f"El promedio por botella es de: {mediaPrecioBotella}")
+lista = ["hola", "mundo", "nose", "que", "poner"]
+cadena = "-".join(lista)
+print(cadena)
