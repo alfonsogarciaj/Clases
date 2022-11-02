6828f5d0312166439c3eb11d5c8d45d312be9b2b
Alfonso García Jorge <alfonsogjorge@gmail.com>
2022-11-02 12:36:46 +0000

    empezando actividad 2.2.

--- Programación/UT2Identificación de los elementos de un programa informático (Python)/pythonProject/Ejercicios1.py
+++ Programación/UT2Identificación de los elementos de un programa informático (Python)/pythonProject/Ejercicios1.py
@@ -1,3 +1,8 @@
-lista = ["hola", "mundo", "nose", "que", "poner"]
-cadena = "-".join(lista)
+cadena = ["hola", "nose", "hola", "que"]
+palabraEliminada = input("Palabra que desea eliminar: ")
+for i in range(len(cadena)-1,-1,-1):
+    if cadena[i] == palabraEliminada:
+        cadena.remove(palabraEliminada)
 print(cadena)
+
+
