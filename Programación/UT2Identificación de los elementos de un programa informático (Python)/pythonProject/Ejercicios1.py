a2b6afd4596d9cda4bd1d42f70e2f48dfe3d21fc
Alfonso García Jorge <alfonsogjorge@gmail.com>
2022-11-05 01:33:13 +0000

    ahi va actividad 2

--- Programación/UT2Identificación de los elementos de un programa informático (Python)/pythonProject/Ejercicios1.py
+++ Programación/UT2Identificación de los elementos de un programa informático (Python)/pythonProject/Ejercicios1.py
@@ -1,7 +1,14 @@
-
-alumnoNombre = input("Nombre del alumno:")
-
-claseDiccionario = {
-    "nombre": alumnoNombre,
-    "nota": nota
-}
+clase = {}
+notasAlumno = []
+for i in range(3):
+    clave = input("Introduce el nombre del alumno: ")
+    contador = int(input("¿Cuantas notas son? "))
+    while contador > 0:
+        valor = int(input("Introduce la nota: "))
+        if valor >= 0 and valor <= 10:
+            notasAlumno.append(valor)
+            contador -= 1
+            clase[clave] = notasAlumno
+        else:
+            print("Tiene que ser entre 0 y 10")
+print(clase)
